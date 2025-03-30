ca456765f7c39370107191292f9c30c1a187db2c
AkshayCodesIt <akshaynatuskar8902@gmail.com>
2025-03-30 20:51:54 +0530

    `Update package-lock.json, package.json, and vite.config.js to include vite-plugin-static-copy`

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: '/artfolio/',
+  plugins: [react(), viteStaticCopy({
+    targets: [
+      { src: 'public/*', dest: '' }
+    ]
+  }), tailwindcss()],
 })
